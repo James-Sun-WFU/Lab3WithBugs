e14b34768d434be6c999e8c8bcc0939195bc7fd6
James-Sun-WFU <78141618+James-Sun-WFU@users.noreply.github.com>
2022-02-11 12:13:45 -0500

    Update lab3.c

--- lab3.c
+++ lab3.c
@@ -6,7 +6,7 @@
  * and provide an error message.
  *
  * @author James Sun {@literal sunh19@wfu.edu}
- * @date Feb.11 th, 2022
+ * @date Feb.7th, 2022
  * @assignment Lab 2
  * @course CSC 250
  **/
@@ -33,7 +33,6 @@ void add_word(char newWord[], struct WordFreq **listPtr, int *nPtr);
 int main(int argc, char* argv[]){
     struct WordFreq* wordListPtr = NULL;
     int numWords = 0;
-    int* numWordsPtr = &numWords;
     int i;
     int fileOK = 1;
     
@@ -43,7 +42,7 @@ int main(int argc, char* argv[]){
         return 1;
     }
 
-    fileOK = processCharacters(&wordListPtr,numWordsPtr,argv[1]);
+    fileOK = processCharacters(&wordListPtr,&numWords,argv[1]);
     if(!fileOK){
         printf("%s could not be opened, and AJ is nice \n", argv[1]);
         return 1;
@@ -53,7 +52,7 @@ int main(int argc, char* argv[]){
      /* print the list to the screen */
 
 
-    for(i = 0; i < numWords; i++)
+    for(i = 0; i < numWords+1; i++)
         printf("list[%d] = %s, count %d \n", i, wordListPtr[i].word, wordListPtr[i].count);
 
     
@@ -71,7 +70,6 @@ int processCharacters(struct WordFreq** wordListPtr, int* numWordsPtr,char filen
 
     FILE *filePtr = fopen(filename, "rie");
     char str[MAX_STRING_SIZE]; /*my only static array*/
-    int ListSize= *numWordsPtr;
     char ch;
     int i;
     int n=0;
@@ -88,6 +86,7 @@ int processCharacters(struct WordFreq** wordListPtr, int* numWordsPtr,char filen
     ch = fgetc(filePtr);
     while(ch != EOF) {
         if(isalpha(ch)){
+            
             ch = tolower(ch);
             str[n]=ch;
             n++;
@@ -96,15 +95,7 @@ int processCharacters(struct WordFreq** wordListPtr, int* numWordsPtr,char filen
             if(n>0){ /*?*/
                 printf("words[%s]\n",str);
                 /*save to array? maybe?*/
-                
-                /*int wordLength = strlen(str);
-                struct WordFreq newWord;
-                newWord.word = (char*)malloc((wordLength+1)*sizeof(char));
-                strcpy(newWord.word, str);
-                newWord.count = 1; */
-
                 add_word(str,wordListPtr,numWordsPtr); 
-                *numWordsPtr++;
                 }
                 n=0;
                 str[n] = '\0';
@@ -116,14 +107,15 @@ int processCharacters(struct WordFreq** wordListPtr, int* numWordsPtr,char filen
     return 1;
 }
 
-/*this function returns the location of the character searched in the list*/
-
 
 /*this function displays the top five characters&digits and their counts. If there are less than five detected, 
 *the display will be adjusted accordingly   */
 void display(int alphaNum){
     
 }
+
+/*this function returns the location of the character searched in the list*/
+
 
 int search(char newWord[] , struct WordFreq** list, int *nPtr){
     int i;
@@ -132,11 +124,11 @@ int search(char newWord[] , struct WordFreq** list, int *nPtr){
         printf("judge is %d\n",judge);
 
         if(judge == 0){
-            printf("return %d\n",i);
+            printf("search returns %d\n",i);
             return i;
         }
     }
-    printf("return null\n");
+    printf("search returns null\n");
     return (-1);
 } 
 
@@ -144,7 +136,7 @@ int search(char newWord[] , struct WordFreq** list, int *nPtr){
 void add_word(char newWord[], struct WordFreq**listPtr, int *nPtr) {
     struct WordFreq *tempList = NULL; /* temp list for copying */
     int i;
-    printf("the size of the list is %d\n",*nPtr);
+    printf("the size of the list before adding is %d\n",*nPtr);
     printf("The word added is %s\n",newWord);
 
     /*for(i=0;i<*nPtr;i++){
@@ -187,8 +179,8 @@ void add_word(char newWord[], struct WordFreq**listPtr, int *nPtr) {
     *listPtr = tempList;
     /* added one more word, increase the count */
     (*nPtr)++;
-
-    printf("words added\n \n");
+    printf("the  size of the list after adding is %d\n",*nPtr);
+    printf("words addition done\n \n");
     /*print the listptr*/
     
     }
